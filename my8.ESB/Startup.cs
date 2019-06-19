1e596d9a9c8dd3b3da03612c6f31a63f5f867bfe
quangphat <phat.dinhquang@haravan.com>
2019-06-19 11:47:11 +0700

    init

---
+++ my8.ESB/Startup.cs
@@ -0,0 +1,65 @@
+﻿using GreenPipes;
+using MassTransit;
+using MassTransit.AspNetCoreIntegration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace my8.ESB
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMassTransit(ConfigureBus());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+        private IBusControl ConfigureBus()
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                var host = sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("quangphat");
+                    h.Password("number8");
+                });
+                sbc.AutoDelete = false;
+                sbc.ReceiveEndpoint(host, "test_queue", ep =>
+                {
+                    ep.Handler<YourMessage>(context =>
+                    {
+                        return Console.Out.WriteLineAsync($"Received: {context.Message.Text}");
+                    });
+                });
+            });
+
+            return bus;
+        }
+
+    }
+    public class YourMessage
+    {
+        public string Text { get; set; }
+    }
+}
