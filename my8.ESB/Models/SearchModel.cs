52f0926523c14dd7c73dc520a4aa8e400f1339c9
quangphat <phat.dinhquang@haravan.com>
2019-07-30 18:58:47 +0700

    SearchModel

--- my8.ESB/Models/SearchModel.cs
+++ my8.ESB/Models/SearchModel.cs
@@ -1,5 +1,7 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using my8.ShareObject.ESB;
+using System;
 
 namespace my8.ESB.Models
 {
@@ -14,5 +16,14 @@ namespace my8.ESB.Models
         public string Title { get; set; }
         public string[] Tags { get; set; }
         public string ProjectId { get; set; }
+        public int Likes { get; set; }
+        public int Views { get; set; }
+        public int Comments { get; set; }
+        public int Shares { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public string FriendlyUrl { get; set; }
+        public string Preview { get; set; }
+        public Author Author { get; set; }
+
     }
 }
